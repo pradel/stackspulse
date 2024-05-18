fee63a225ac3e78d98de565ec4271078e56673a0
Léo Pradel <pradel.leo@gmail.com>
2024-05-18 18:59:19 +0200

    chore: upgrade deps

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/db/schema.ts",
   out: "./drizzle",
-  driver: "better-sqlite",
+  dialect: "sqlite",
   dbCredentials: {
     url: "./sqlite.db",
   },
