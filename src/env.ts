042aa6be73af1f1432ff3aecb11238609a703f59
Léo Pradel <pradel.leo@gmail.com>
2024-05-19 13:24:52 +0200

    feat: setup sentry

--- src/env.ts
+++ src/env.ts
@@ -14,9 +14,11 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_BASE_URL: z.string().min(1),
     NEXT_PUBLIC_FATHOM_ID: z.string().optional(),
+    NEXT_PUBLIC_SENTRY_DSN: z.string().optional(),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     NEXT_PUBLIC_FATHOM_ID: process.env.NEXT_PUBLIC_FATHOM_ID,
+    NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
   },
 });
