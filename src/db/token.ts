0562fa407f62e564e0970c26339224b7c927c185
Léo Pradel <pradel.leo@gmail.com>
2024-05-19 11:10:46 +0200

    run contract calls

--- src/db/token.ts
+++ src/db/token.ts
@@ -26,23 +26,25 @@ export const getOrInsertToken = async (tokenId: string) => {
 
     const smartContract = tokenId.split("::")[0];
     const [contractAddress, contractName] = smartContract.split(".");
-    const resultDecimals = await callReadOnlyFunction({
-      contractAddress,
-      contractName,
-      functionName: "get-decimals",
-      functionArgs: [],
-      network: "mainnet",
-      senderAddress: "SP2EVYKET55QH40RAZE5PVZ363QX0X6BSRP4C7H0W",
-    });
 
-    const resultSymbol = await callReadOnlyFunction({
-      contractAddress,
-      contractName,
-      functionName: "get-symbol",
-      functionArgs: [],
-      network: "mainnet",
-      senderAddress: "SP2EVYKET55QH40RAZE5PVZ363QX0X6BSRP4C7H0W",
-    });
+    const [resultDecimals, resultSymbol] = await Promise.all([
+      callReadOnlyFunction({
+        contractAddress,
+        contractName,
+        functionName: "get-decimals",
+        functionArgs: [],
+        network: "mainnet",
+        senderAddress: "SP2EVYKET55QH40RAZE5PVZ363QX0X6BSRP4C7H0W",
+      }),
+      callReadOnlyFunction({
+        contractAddress,
+        contractName,
+        functionName: "get-symbol",
+        functionArgs: [],
+        network: "mainnet",
+        senderAddress: "SP2EVYKET55QH40RAZE5PVZ363QX0X6BSRP4C7H0W",
+      }),
+    ]);
 
     token = {
       id: tokenId,
