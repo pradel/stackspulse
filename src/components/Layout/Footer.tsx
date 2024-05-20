27ede23a977e20470ac44d63c8c02951564b4a5d
Léo Pradel <pradel.leo@gmail.com>
2024-05-20 12:24:21 +0200

    feat: add author to footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,12 +1,12 @@
 import { appConfig } from "@/appConfig";
-import { Container, IconButton } from "@radix-ui/themes";
+import { Container, IconButton, Link, Text } from "@radix-ui/themes";
 import { IconBrandGithub, IconBrandX } from "@tabler/icons-react";
 
 export const Footer = () => {
   return (
     <footer>
       <Container size="2" className="px-4 py-10">
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-between">
           <div className="flex items-center gap-5">
             <IconButton size="2" color="gray" variant="ghost" asChild>
               <a href={appConfig.github} target="_blank" rel="noreferrer">
@@ -19,6 +19,12 @@ export const Footer = () => {
               </a>
             </IconButton>
           </div>
+          <Text as="p" size="1" color="gray">
+            Build by{" "}
+            <Link href="https://www.leopradel.com/" target="_blank">
+              @leopradel
+            </Link>
+          </Text>
         </div>
       </Container>
     </footer>
