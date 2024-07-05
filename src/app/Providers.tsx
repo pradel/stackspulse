93e76cf5bf9132ea55f6d99baf83a9c0a18bc2fb
Léo Pradel <pradel.leo@gmail.com>
2024-07-05 11:43:25 +0200

    Update Providers.tsx

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -2,7 +2,14 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // With SSR, we set staleTime to avoid refetching immediately on the client
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 export const Providers = ({
   children,
