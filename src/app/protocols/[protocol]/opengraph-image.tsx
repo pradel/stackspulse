e1a5d2e75fa6b99f6d13fbb205e9a4f79c512a16
Léo Pradel <pradel.leo@gmail.com>
2024-07-12 13:31:47 +0200

    eslint

--- src/app/protocols/[protocol]/opengraph-image.tsx
+++ src/app/protocols/[protocol]/opengraph-image.tsx
@@ -37,6 +37,7 @@ export default async function Image({ params }: PageProps) {
       }}
     >
       <div tw="flex items-center">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           tw="mr-4"
           src={`${env.NEXT_PUBLIC_BASE_URL}/protocols/${protocol}.png`}
