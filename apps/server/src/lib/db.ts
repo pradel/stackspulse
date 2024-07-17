3ab9a33570519d620f4bc657085ce48150d16f30
Léo Pradel <pradel.leo@gmail.com>
2024-07-17 20:15:29 +0200

    get it to work

--- apps/server/src/lib/db.ts
+++ apps/server/src/lib/db.ts
@@ -1,5 +1,5 @@
-import { env } from "@/env";
 import postgres from "postgres";
+import { env } from "~/env";
 
 // Postgres.js doesn't support the schema public parameter
 const databaseUrl = env.DATABASE_URL.replace("?schema=public", "");
