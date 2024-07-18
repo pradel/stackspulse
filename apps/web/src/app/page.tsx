af4ce6e419c0420008d53f3e97b0f30a8a2d41d3
Léo Pradel <pradel.leo@gmail.com>
2024-07-18 23:30:39 +0200

    client

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,17 +1,8 @@
+import { HomeStats } from "@/components/Home/HomeStats";
 import { HomeTransactions } from "@/components/Home/HomeTransactions";
 import { TopProtocolsBarList } from "@/components/Stats/TopProtocolsBarList";
-import { TransactionRow } from "@/components/Transaction/TransactionRow";
-import { getTransactions, getTransactionsStats } from "@/db/transactions";
-import {
-  Card,
-  Container,
-  Heading,
-  Separator,
-  Text,
-  Tooltip,
-} from "@radix-ui/themes";
-import { IconInfoCircle } from "@tabler/icons-react";
-import { Fragment, Suspense } from "react";
+import { Container, Heading } from "@radix-ui/themes";
+import { Suspense } from "react";
 
 export const dynamic = "force-dynamic";
 // TODO
@@ -19,11 +10,6 @@ export const dynamic = "force-dynamic";
 // export const revalidate = 60;
 
 export default async function HomePage() {
-  const [transactions, stats] = await Promise.all([
-    getTransactions(),
-    getTransactionsStats(),
-  ]);
-
   return (
     <Container size="2" className="px-4 pt-10">
       <div>
@@ -35,40 +21,9 @@ export default async function HomePage() {
         </Heading>
       </div>
 
-      <div className="mt-10 grid grid-cols-2 gap-5">
-        <Card size="2">
-          <Text
-            className="flex items-center gap-1"
-            as="div"
-            size="2"
-            color="gray"
-          >
-            Total Transactions
-            <Tooltip content="Total number of transactions that interacted with the protocols supported by stackspulse">
-              <IconInfoCircle size={14} />
-            </Tooltip>
-          </Text>
-          <Text as="div" mt="2" size="5" weight="medium">
-            {stats.count.toLocaleString("en-US")}
-          </Text>
-        </Card>
-        <Card size="2">
-          <Text
-            className="flex items-center gap-1"
-            as="div"
-            size="2"
-            color="gray"
-          >
-            Unique addresses
-            <Tooltip content="Total number of unique addresses that interacted with the protocols supported by stackspulse">
-              <IconInfoCircle size={14} />
-            </Tooltip>
-          </Text>
-          <Text as="div" mt="2" size="5" weight="medium">
-            {stats.uniqueSenders.toLocaleString("en-US")}
-          </Text>
-        </Card>
-      </div>
+      <Suspense>
+        <HomeStats />
+      </Suspense>
 
       <TopProtocolsBarList />
 
