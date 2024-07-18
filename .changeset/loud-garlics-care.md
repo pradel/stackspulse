fd270bec388446dad2c1b0174d978d9492d87876
Léo Pradel <pradel.leo@gmail.com>
2024-07-18 21:52:55 +0200

    changeset

---
+++ .changeset/loud-garlics-care.md
@@ -0,0 +1,5 @@
+---
+"stackspulse": minor
+---
+
+Take protocols stats from API.
---
+++ apps/server/src/api/transactions/stats/index.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { sql } from "~/lib/db";
+import { getValidatedQueryZod } from "~/lib/nitro";
+
+type Protocol = "TODO";
+const protocols = ["TODO"] as const;
+
+const transactionStatsRouteSchema = z.object({
+  protocol: z.enum(protocols).optional(),
+});
+
+export type TransactionStatsRouteResponse = {
+  count: number;
+  unique_senders: number;
+};
+
+export default defineEventHandler(async (event) => {
+  const query = await getValidatedQueryZod(event, transactionStatsRouteSchema);
+
+  let protocolCondition = "";
+  if (query.protocol) {
+    protocolCondition = `AND dapps.id = '${query.protocol}'`;
+  }
+
+  const result = await sql`
+SELECT
+  COUNT(txs.id) AS count,
+  COUNT(DISTINCT sender_address) AS unique_senders
+FROM
+    txs
+JOIN
+    dapps ON txs.contract_call_contract_id = ANY (dapps.contracts)
+WHERE
+  txs.type_id = 2
+  ${sql.unsafe(protocolCondition)}
+  `;
+
+  const stats: TransactionStatsRouteResponse = {
+    count: Number.parseInt(result[0].count),
+    unique_senders: Number.parseInt(result[0].unique_senders),
+  };
+
+  return stats;
+});
