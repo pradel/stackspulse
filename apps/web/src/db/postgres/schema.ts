5b887d72697973802f3698329859b701108f5ea6
Léo Pradel <pradel.leo@gmail.com>
2024-07-19 13:09:42 +0200

    migrate prisma

--- apps/web/src/db/postgres/schema.ts
+++
@@ -1,6 +0,0 @@
-import { pgTable, text } from "drizzle-orm/pg-core";
-
-export const dapps = pgTable("dapps", {
-  id: text("id").primaryKey(),
-  contracts: text("contracts").array().notNull(),
-});
