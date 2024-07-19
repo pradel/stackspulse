ce6dfd3759bc29c87adbb32f5c319d18c3b959d6
Léo Pradel <pradel.leo@gmail.com>
2024-07-19 11:55:26 +0200

    feat: migrate txs unique senders

---
+++ apps/server/src/api/transactions/unique-senders/index.ts
@@ -0,0 +1,49 @@
+/**
+select "protocol", strftime('%Y-%m', timestamp, 'unixepoch') as month, count(distinct "sender")
+order by "transaction"."timestamp" asc
+**/
+
+import { z } from "zod";
+import { sql } from "~/lib/db";
+import { getValidatedQueryZod } from "~/lib/nitro";
+import { protocols } from "~/lib/protocols";
+
+const transactionUniqueSendersRouteSchema = z.object({
+  protocol: z.enum(protocols),
+});
+
+type TransactionUniqueSendersRouteResponse = {
+  month: string;
+  unique_senders: number;
+}[];
+
+export default defineEventHandler(async (event) => {
+  const query = await getValidatedQueryZod(
+    event,
+    transactionUniqueSendersRouteSchema,
+  );
+
+  const result = await sql`
+SELECT
+  COUNT(DISTINCT sender_address) AS unique_senders,
+  to_char(to_timestamp(txs.block_time), 'YYYY-MM') as month
+FROM
+  txs
+JOIN
+  dapps ON txs.contract_call_contract_id = ANY (dapps.contracts)
+WHERE
+  txs.type_id = 2
+  AND dapps.id = ${query.protocol}
+GROUP BY
+  to_char(to_timestamp(txs.block_time), 'YYYY-MM')
+ORDER BY
+  month ASC
+  `;
+
+  const stats: TransactionUniqueSendersRouteResponse = result.map((row) => ({
+    month: row.month,
+    unique_senders: Number.parseInt(row.unique_senders),
+  }));
+
+  return stats;
+});
