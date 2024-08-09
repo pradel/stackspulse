1700e6fd7b12353c790c1a9cfed95260ca45c86f
Léo Pradel <pradel.leo@gmail.com>
2024-08-09 11:46:11 +0200

    revert

--- apps/web/src/lib/actions.ts
+++ apps/web/src/lib/actions.ts
@@ -9,17 +9,6 @@ export const actions = [
   // StackingDAO
   "stackingdao-deposit",
   "stackingdao-withdraw",
-  // Zest
-  "withdraw",
-  "supply",
-  "repay",
-  "borrow",
-  "set-user-use-reserve-as-collateral",
-  "borrow-call",
-  "repay-call",
-  "supply-call",
-  "withdraw-call",
-  "set-user-use-reserve-as-collateral-call",
 ] as const;
 
 export type Action = (typeof actions)[number];
@@ -50,39 +39,6 @@ export const actionInfo: {
   "stackingdao-withdraw": {
     label: "Withdraw",
   },
-  /**
-   * Zest
-   */
-  withdraw: {
-    label: "Withdraw",
-  },
-  supply: {
-    label: "Supply",
-  },
-  repay: {
-    label: "Repay",
-  },
-  borrow: {
-    label: "Borrow",
-  },
-  "set-user-use-reserve-as-collateral": {
-    label: "Flag Collateral",
-  },
-  "borrow-call": {
-    label: "Borrow",
-  },
-  "repay-call": {
-    label: "Repay",
-  },
-  "supply-call": {
-    label: "Supply",
-  },
-  "withdraw-call": {
-    label: "Withdraw",
-  },
-  "set-user-use-reserve-as-collateral-call": {
-    label: "Flag Collateral",
-  },
 } as const;
 
 export const protocolsActions: {
@@ -94,16 +50,5 @@ export const protocolsActions: {
   stackingdao: ["stackingdao-deposit", "stackingdao-withdraw"],
   stackswap: ["swap"],
   velar: ["swap"],
-  zest: [
-    "withdraw",
-    "supply",
-    "repay",
-    "borrow",
-    "set-user-use-reserve-as-collateral",
-    "borrow-call",
-    "repay-call",
-    "supply-call",
-    "withdraw-call",
-    "set-user-use-reserve-as-collateral-call",
-  ],
+  zest: [],
 } as const;
