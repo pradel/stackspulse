449f8f957bc5400f8d203ecdfb54cc93606351ab
Léo Pradel <pradel.leo@gmail.com>
2024-08-11 15:55:31 +0200

    new fields

--- apps/server/src/api/transactions/index.get.ts
+++ apps/server/src/api/transactions/index.get.ts
@@ -13,6 +13,8 @@ type TransactionsRouteResponse = {
   tx_id: string;
   contract_call_contract_id: string;
   contract_call_function_name: string;
+  block_height: number;
+  block_time: number;
 }[];
 
 export default defineCachedEventHandler(async (event) => {
@@ -29,13 +31,17 @@ export default defineCachedEventHandler(async (event) => {
       tx_id: Buffer;
       contract_call_contract_id: string;
       contract_call_function_name: string;
+      block_height: number;
+      block_time: number;
     }[]
   >`
 SELECT
     dapps.id as protocol,
     tx_id,
     contract_call_contract_id,
-    contract_call_function_name
+    contract_call_function_name,
+    block_height,
+    block_time
 FROM
     txs
 JOIN
@@ -43,7 +49,10 @@ JOIN
 WHERE
     txs.type_id = 2
     ${sql.unsafe(protocolCondition)}
-LIMIT 100
+ORDER BY
+    block_height DESC,
+    tx_index DESC
+LIMIT 50
   `;
 
   const formattedResult: TransactionsRouteResponse = result.map((row) => ({
@@ -51,6 +60,8 @@ LIMIT 100
     tx_id: `0x${row.tx_id.toString("hex")}`,
     contract_call_contract_id: row.contract_call_contract_id,
     contract_call_function_name: row.contract_call_function_name,
+    block_height: row.block_height,
+    block_time: row.block_time,
   }));
 
   return formattedResult;
