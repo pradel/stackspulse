7cf490753b1ead37623e73713a8a155ebca620a2
Léo Pradel <pradel.leo@gmail.com>
2024-09-27 22:54:40 +0200

    update logic

--- apps/server/src/api/tokens/holders.get.ts
+++ apps/server/src/api/tokens/holders.get.ts
@@ -1,44 +1,37 @@
 import { z } from "zod";
-import { sql } from "~/db/db";
 import { apiCacheConfig } from "~/lib/api";
 import { getValidatedQueryZod } from "~/lib/nitro";
+import { stacksClient } from "~/lib/stacks";
 
-const tokensTransactionVolumeRouteSchema = z.object({
+const tokensHoldersRouteSchema = z.object({
   token: z.string(),
   limit: z.number().min(1).max(100).optional(),
   offset: z.number().min(0).optional(),
 });
 
+type TokensHoldersRouteResponse = {
+  total_supply: string;
+  total: number;
+  results: {
+    address: string;
+    balance: string;
+  }[];
+};
+
 export default defineCachedEventHandler(async (event) => {
-  const query = await getValidatedQueryZod(
-    event,
-    tokensTransactionVolumeRouteSchema,
-  );
+  const query = await getValidatedQueryZod(event, tokensHoldersRouteSchema);
 
   const limit = query.limit ?? 20;
   const offset = query.offset ?? 0;
 
-  const result = await sql`
-WITH totals AS (
-    SELECT
-        SUM(balance) AS total,
-        COUNT(*)::int AS total_count
-    FROM ft_balances
-    WHERE token = ${query.token}
-)
-SELECT
-    fb.address,
-    fb.balance,
-    ts.total AS total_supply,
-    ts.total_count AS count,
-    ROUND((fb.balance::numeric / ts.total::numeric) * 100, 2) AS percentage
-FROM ft_balances fb
-CROSS JOIN totals ts
-WHERE fb.token = ${query.token}
-ORDER BY fb.balance DESC
-LIMIT ${limit}
-OFFSET ${offset}
-  `;
+  const data: TokensHoldersRouteResponse = (
+    await stacksClient.GET("/extended/v1/tokens/ft/{token}/holders", {
+      params: {
+        path: { token: query.token },
+        query: { limit, offset },
+      },
+    })
+  ).data;
 
-  return result;
+  return data;
 }, apiCacheConfig);
