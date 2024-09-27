99d8f1461069d2fb8adc2b4c3b1c3cf8f6cafca4
Léo Pradel <pradel.leo@gmail.com>
2024-09-27 23:21:09 +0200

    fix

--- apps/web/src/components/Token/TokenInfo.tsx
+++ apps/web/src/components/Token/TokenInfo.tsx
@@ -7,16 +7,19 @@ interface TokenInfoProps {
 }
 
 export const TokenInfo = ({ tokenInfo }: TokenInfoProps) => {
+  const tokenImage = tokenInfo.image_thumbnail_uri || tokenInfo.image_uri;
   return (
     <div className="flex items-start gap-5">
-      <Image
-        className="rounded-full"
-        src={tokenInfo.image_thumbnail_uri}
-        alt={`${tokenInfo.name} logo`}
-        width={50}
-        height={50}
-        priority
-      />
+      {tokenImage ? (
+        <Image
+          className="rounded-full"
+          src={tokenImage}
+          alt={`${tokenInfo.name} logo`}
+          width={50}
+          height={50}
+          priority
+        />
+      ) : null}
       <div>
         <Heading as="h1" size="5" color="gray" highContrast>
           {tokenInfo.symbol} - {tokenInfo.name}
