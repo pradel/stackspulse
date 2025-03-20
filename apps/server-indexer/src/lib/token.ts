eec0e834e895431e049e29fc039390f7f6ea1ecc
Léo Pradel <pradel.leo@gmail.com>
2025-03-20 23:55:55 +0100

    more

--- apps/server-indexer/src/lib/token.ts
+++ apps/server-indexer/src/lib/token.ts
@@ -9,6 +9,31 @@ export const getOrCreateToken = async (tokenAddress: string) => {
   if (token) return token;
 
   const [contractAddress, contractName] = tokenAddress.split(".");
+
+  const symbolResult = await fetchCallReadOnlyFunction({
+    contractAddress,
+    contractName,
+    functionName: "get-symbol",
+    functionArgs: [],
+    senderAddress: "SP2X0TZ59D5SZ8ACQ6YMCHHNR2ZN51Z32E2CJ173",
+  });
+  if (symbolResult.type !== "ok" || symbolResult.value.type !== "ascii") {
+    throw new Error("Failed to fetch symbol");
+  }
+  const symbol = symbolResult.value.value;
+
+  const nameResult = await fetchCallReadOnlyFunction({
+    contractAddress,
+    contractName,
+    functionName: "get-name",
+    functionArgs: [],
+    senderAddress: "SP2X0TZ59D5SZ8ACQ6YMCHHNR2ZN51Z32E2CJ173",
+  });
+  if (nameResult.type !== "ok" || nameResult.value.type !== "ascii") {
+    throw new Error("Failed to fetch name");
+  }
+  const name = nameResult.value.value;
+
   const decimalsResult = await fetchCallReadOnlyFunction({
     contractAddress,
     contractName,
@@ -24,6 +49,8 @@ export const getOrCreateToken = async (tokenAddress: string) => {
   const newToken = await prisma.token.create({
     data: {
       id: tokenAddress,
+      symbol,
+      name,
       decimals,
       txCount: 0,
       poolCount: 0,
