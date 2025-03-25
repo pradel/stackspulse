579452f1855de4c17b6af8553e4e339dc1fe388c
Léo Pradel <pradel.leo@gmail.com>
2025-03-25 12:07:39 +0100

    prisma fix

--- apps/server-indexer/src/lib/prisma.ts
+++ apps/server-indexer/src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "~~/generated/client";
+import { PrismaClient } from "@prisma/client-generated";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
