e462e75452fe9ce40b58bb394ce417e74a351cba
Léo Pradel <pradel.leo@gmail.com>
2025-03-25 21:57:46 +0100

    Update create-pool.ts

--- apps/server-indexer/src/dapps/alex-v2/create-pool.ts
+++ apps/server-indexer/src/dapps/alex-v2/create-pool.ts
@@ -3,11 +3,25 @@ import { prisma } from "~/lib/prisma";
 import { getOrCreateToken } from "~/lib/token";
 
 interface PoolCreatedEvent {
+  action: "created";
+  data: {
+    "balance-x": number;
+    "balance-y": number;
+    "end-block": number;
+    "fee-rate-x": number;
+    "fee-rate-y": number;
+    "fee-rebate": number;
+    "pool-id": number;
+    "pool-owner": string;
+    "start-block": number;
+    "threshold-x": number;
+    "threshold-y": number;
+    "total-supply": number;
+  };
+  factor: number;
+  object: "pool";
   "token-x": string;
   "token-y": string;
-  data: {
-    "pool-id": number;
-  };
 }
 // Some blocks that can be tested 155991, 155995, 780604
 // Example: https://explorer.hiro.so/txid/0x4e43f10dfd1bc243eccd454afb96e9f027fe2afb665c63fc0e4c00c050c07284?chain=mainnet
