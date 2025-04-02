b1867f368b8c667215575fbcfcde230c24e32d93
Léo Pradel <pradel.leo@gmail.com>
2025-04-02 20:42:11 +0200

    more

--- apps/server-indexer/src/dapps/alex-v2/dapp.ts
+++ apps/server-indexer/src/dapps/alex-v2/dapp.ts
@@ -2,8 +2,15 @@ import type { StacksTransaction } from "@hirosystems/chainhook-client";
 import { prisma } from "~/lib/prisma";
 
 const contracts = [
+  // alex-v2
   "SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM.amm-registry-v2-01",
   "SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM.amm-pool-v2-01",
+  "SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM.amm-vault-v2-01",
+  // alex-v1
+  "SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.amm-swap-pool-v1-1",
+  "SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.swap-helper-v1-01",
+  "SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.swap-helper-v1-02",
+  "SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.swap-helper-v1-03",
 ];
 
 export const alexDapp = {
