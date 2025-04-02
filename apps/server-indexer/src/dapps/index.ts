90046a1e08d57c018147fe13a06517370c21ccec
Léo Pradel <pradel.leo@gmail.com>
2025-04-02 23:14:44 +0200

    bitflow

--- apps/server-indexer/src/dapps/index.ts
+++ apps/server-indexer/src/dapps/index.ts
@@ -1,6 +1,7 @@
 import type { Dapp } from "~/routes/api/chainhook/webhook.post";
 import { alexDapp } from "./alex";
 import { arkadikoDapp } from "./arkadiko";
+import { bitflowDapp } from "./bitflow";
 import { hermeticaDapp } from "./hermetica";
 import { stackingdaoDapp } from "./stackingdao";
 import { stackswapDapp } from "./stackswap";
@@ -8,6 +9,7 @@ import { stackswapDapp } from "./stackswap";
 export const dapps: Dapp[] = [
   alexDapp,
   arkadikoDapp,
+  bitflowDapp,
   hermeticaDapp,
   stackingdaoDapp,
   stackswapDapp,
