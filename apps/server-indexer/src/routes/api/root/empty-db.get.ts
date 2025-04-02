1804837529806bad2aa23060e8e912f58016b464
Léo Pradel <pradel.leo@gmail.com>
2025-04-02 22:29:31 +0200

    more

--- apps/server-indexer/src/routes/api/root/empty-db.get.ts
+++ apps/server-indexer/src/routes/api/root/empty-db.get.ts
@@ -3,8 +3,10 @@ import { prisma } from "~/lib/prisma";
 // TODO delete this file after first version is released
 export default defineEventHandler(async () => {
   await prisma.trade.deleteMany({ where: {} });
+  await prisma.pool.deleteMany({ where: {} });
+  await prisma.token.deleteMany({ where: {} });
   await prisma.transaction.deleteMany({ where: {} });
   await prisma.block.deleteMany({ where: {} });
-
+  await prisma.dapp.deleteMany({ where: {} });
   return { ok: true };
 });
