d07b6acb6dc43713bd1683a88aabf7adeea8c54f
Léo Pradel <pradel.leo@gmail.com>
2025-05-10 19:06:44 +0200

    more

--- apps/server/src/api/transactions/index.get.ts
+++ apps/server/src/api/transactions/index.get.ts
@@ -1,9 +1,10 @@
+import { Prisma } from "@prisma/client";
 import type { ContractCallTransaction } from "@stacks/stacks-blockchain-api-types";
 import { type Protocol, protocols } from "@stackspulse/protocols";
 import { z } from "zod";
-import { sql } from "~/db/db";
 import { apiCacheConfig } from "~/lib/api";
 import { getValidatedQueryZod } from "~/lib/nitro";
+import { prisma } from "~/lib/prisma";
 import { stacksClient } from "~/lib/stacks";
 
 const transactionsRouteSchema = z.object({
@@ -17,33 +18,32 @@ type TransactionsRouteResponse = (ContractCallTransaction & {
 export default defineCachedEventHandler(async (event) => {
   const query = await getValidatedQueryZod(event, transactionsRouteSchema);
 
-  let protocolCondition = "";
-  if (query.protocol) {
-    protocolCondition = `AND dapps.id = '${query.protocol}'`;
-  }
+  const protocolCondition = query.protocol
+    ? Prisma.sql`WHERE dapps.id = ${query.protocol}`
+    : Prisma.sql``;
 
-  const result = await sql<
+  const result = await prisma.$queryRaw<
     {
       protocol: Protocol;
       tx_id: Buffer;
     }[]
   >`
-SELECT
-    dapps.id as protocol,
-    tx_id
-FROM
-    txs
-JOIN
-    dapps ON txs.contract_call_contract_id = ANY (dapps.contracts)
-WHERE
-    txs.type_id = 2
-    ${sql.unsafe(protocolCondition)}
-    AND canonical = TRUE
-    AND microblock_canonical = TRUE
-ORDER BY
-    block_height DESC,
-    tx_index DESC
-LIMIT 50
+    SELECT
+        dapps.id as protocol,
+        tx_id
+    FROM
+        txs
+    JOIN
+        dapps ON txs.contract_call_contract_id = ANY (dapps.contracts)
+    WHERE
+        txs.type_id = 2
+        ${protocolCondition}
+        AND canonical = TRUE
+        AND microblock_canonical = TRUE
+    ORDER BY
+        block_height DESC,
+        tx_index DESC
+    LIMIT 50
   `;
 
   const formattedResult = result.map((r) => ({
