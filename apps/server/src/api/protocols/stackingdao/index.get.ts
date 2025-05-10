8e570a74c648487c963252316810f9ff7481d1e5
Léo Pradel <pradel.leo@gmail.com>
2025-05-10 18:48:56 +0200

    Update index.get.ts

--- apps/server/src/api/protocols/stackingdao/index.get.ts
+++ apps/server/src/api/protocols/stackingdao/index.get.ts
@@ -1,5 +1,5 @@
-import { sql } from "~/db/db";
 import { apiCacheConfig } from "~/lib/api";
+import { prisma } from "~/lib/prisma";
 
 type StackingDAOProtocolStatsResponse = {
   month: string;
@@ -8,7 +8,13 @@ type StackingDAOProtocolStatsResponse = {
 }[];
 
 export default defineCachedEventHandler(async (event) => {
-  const result = await sql`
+  const result = await prisma.$queryRaw<
+    {
+      month: string;
+      deposits: bigint;
+      withdrawals: bigint;
+    }[]
+  >`
 WITH monthly_blocks AS (
     SELECT
         DATE_TRUNC('month', TO_TIMESTAMP(burn_block_time)) AS month,
@@ -26,16 +32,16 @@ deposits AS (
     SELECT
         mb.month,
         SUM(se.amount) AS deposits
-    FROM 
+    FROM
         monthly_blocks mb
-    JOIN 
+    JOIN
         stx_events se ON se.block_height BETWEEN mb.min_block_height AND mb.max_block_height
     WHERE
         se.recipient = 'SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.reserve-v1'
         AND se.sender NOT LIKE 'SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG%'
         AND canonical = TRUE
         AND microblock_canonical = TRUE
-    GROUP BY 
+    GROUP BY
         mb.month
 ),
 
@@ -74,8 +80,8 @@ ORDER BY
     // format of the month is "2021-08-01 00:00:00+00"
     // we want to output "2021-08"
     month: row.month.slice(0, 7),
-    deposits: Number.parseInt(row.deposits),
-    withdrawals: Number.parseInt(row.withdrawals),
+    deposits: Number.parseInt(row.deposits.toString()),
+    withdrawals: Number.parseInt(row.withdrawals.toString()),
   }));
 
   return stats;
