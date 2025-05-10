cae570b09b41426773ef01bb9f0e99f1feb6763a
Léo Pradel <pradel.leo@gmail.com>
2025-05-10 17:37:13 +0200

    feat: use prisma for queries

--- apps/server/src/api/transactions/stats/index.ts
+++ apps/server/src/api/transactions/stats/index.ts
@@ -1,8 +1,9 @@
+import { Prisma } from "@prisma/client";
 import { protocols } from "@stackspulse/protocols";
-import { z } from "zod";
-import { sql } from "~/db/db";
+import { bigint, z } from "zod";
 import { apiCacheConfig } from "~/lib/api";
 import { getValidatedQueryZod } from "~/lib/nitro";
+import { prisma } from "~/lib/prisma";
 
 const transactionStatsRouteSchema = z.object({
   protocol: z.enum(protocols).optional(),
@@ -21,11 +22,20 @@ export default defineCachedEventHandler(async (event) => {
     protocolContractsCondition = `WHERE dapps.id = '${query.protocol}'`;
   }
 
-  const result = await sql`
+  console.log("test");
+
+  const result = await prisma.$queryRaw<
+    [
+      {
+        count: bigint;
+        unique_senders: bigint;
+      },
+    ]
+  >`
 WITH protocol_contracts AS (
     SELECT UNNEST(contracts) AS contract_address
     FROM dapps
-    ${sql.unsafe(protocolContractsCondition)}
+    ${Prisma.sql([protocolContractsCondition])}
 ),
 
 address_txs AS (
@@ -69,9 +79,11 @@ JOIN
   AND atxs.microblock_hash = txs.microblock_hash
   `;
 
+  console.log("results", result);
+
   const stats: TransactionStatsRouteResponse = {
-    count: Number.parseInt(result[0].count),
-    unique_senders: Number.parseInt(result[0].unique_senders),
+    count: Number.parseInt(result[0].count.toString()),
+    unique_senders: Number.parseInt(result[0].unique_senders.toString()),
   };
 
   return stats;
